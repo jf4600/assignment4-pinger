5df0a1aeb3a3964bbbc6b2a0862be2b46d7b7ce6
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-04-10 16:14:11 -0400

    pushing

--- solution.py
+++ solution.py
@@ -5,10 +5,10 @@ import struct
 import time
 import select
 import binascii
+import statistics
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
-
 
 def checksum(string):
     csum = 0
@@ -118,21 +118,21 @@ def ping(host, timeout=1):
     
     #Send ping requests to a server separated by approximately one second
     #Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
-    
+
     val = []
     for i in range(0,4): #Four pings will be sent (loop runs for i=0, 1, 2, 3)
         delay = doOnePing(dest, timeout)
         print(delay)
         time.sleep(1)  # one second
-        val.append(i)
+        val.append(delay)
 
     #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
     if len(val) > 0:
-        packet_min = min(val)
-        packet_avg = sum(val) / len(val)
-        packet_max = max(val)
-        stdev_var = list(val)
-        vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)), str(round(stdev(stdev_var), 8))]
+        packet_min = min(val) *1000
+        packet_avg = sum(val) / len(val) *1000
+        packet_max = max(val) *1000
+        stdev_var = list(val) *1000
+        vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)), str(round(statistics.stdev(stdev_var), 8))]
         print(packet_max, packet_avg, packet_min, stdev_var)
     else:
         vars = ['0', '0.0', '0', '0.0']
